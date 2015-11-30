ddff00674c09bc1e14f435a59447c8ab45e3ec35
josmiranda <opti545@gmail.com>
2015-11-30 13:30:39 -0600

    WIP: Both arms can move now

--- baxter_builder/scripts/baxter_move_arm.py
+++ baxter_builder/scripts/baxter_move_arm.py
@@ -12,53 +12,87 @@ import baxter_interface
 def init_node():
     global robot
     global scene
-    global group
-    global gripper
-    print "Starting node"
+    global both_group
+    global left_group
+    global right_group
+    global plan_left, plan_right
+    global pose_left, pose_right, pose_both
+
     mc.roscpp_initialize(sys.argv)
     rospy.init_node('move_arm_node')
-    print "move_arm_node should be initialized"
-    print "Initializing Robot Commander"
     robot = mc.RobotCommander()
 
-    print "Initializing Planning Scene Interface"
     scene = mc.PlanningSceneInterface()
-    print "Let's move left arm first"
-    group = mc.MoveGroupCommander("left_arm")
-    gripper = baxter_interface.Gripper('left')
+    both_group = mc.MoveGroupCommander("both_arms")
+    left_group = mc.MoveGroupCommander("left_arm")
+    right_group = mc.MoveGroupCommander("right_arm")
+
+    pose_left = geometry_msgs.msg.Pose()
+    pose_right = geometry_msgs.msg.Pose()
+    pose_both = geometry_msgs.msg.Pose()
+
+
+    #Get the arms to the position needed
+    rospy.loginfo('For left')
+    pose_left.orientation.x = 1
+    pose_left.position.x = 0.7
+    pose_left.position.y = 0.4
+    pose_left.position.z = 0.3
+    left_group.set_pose_target(pose_left)
+
+    plan_left = left_group.plan()
+    rospy.sleep(2)
+
+    rospy.loginfo('For right_group')
+    pose_right.orientation.x = 1
+    pose_right.position.x = 0.7
+    pose_right.position.y = -0.4
+    pose_right.position.z = 0.3
+    right_group.set_pose_target(pose_right)
+
+    plan_right = right_group.plan()
+    rospy.sleep(2)
+
+    both_group.set_pose_target(pose_right,'right_gripper')
+    both_group.set_pose_target(pose_left, 'left_gripper')
+
+    plan_both = both_group.plan()
+    rospy.sleep(2)
+    both_group.go(wait = True)
+
 
 def move_arm_node():
-    print "Calling init_node()"
+    rospy.loginfo('Calling init_node()')
     init_node()
-    print "move_arm_node should be initialized"
+    rospy.loginfo('move_arm_node should be initialized')
     rospy.sleep(1)
-    robot_pose = geometry_msgs.msg.Pose()
 
     while not rospy.is_shutdown():
-        robot_pose.orientation.x = 1
+        rospy.loginfo('For left')
+        pose_left.orientation.x = 1
+        pose_left.position.x = 0.7
+        pose_left.position.y = 0.4
+        pose_left.position.z = 0.3
+        left_group.set_pose_target(pose_left)
 
-        robot_pose.position.x = 0.7
-        robot_pose.position.y = 0.5
-        robot_pose.position.z = 0.3
-
-        group.set_pose_target(robot_pose)
-        plan1 = group.plan()
-        rospy.sleep(3)
-        group.go(wait=True)
-
-        robot_pose.orientation.x = 1
-
-        robot_pose.position.x = 0.83
-        robot_pose.position.y = 0.1
-        robot_pose.position.z = 0.07
-
-        group.set_pose_target(robot_pose)
-        plan1 = group.plan()
-        rospy.sleep(3)
-        group.go(wait=True)
-
+        plan_left = left_group.plan()
+        rospy.sleep(2)
+        
+        left_group.go(wait = True)
         
         
+        rospy.loginfo('For left')
+        pose_left.orientation.x = 1
+        pose_left.position.x = 0.83
+        pose_left.position.y = 0.1
+        pose_left.position.z = 0.07
+        left_group.set_pose_target(pose_left)
+
+        plan_left = left_group.plan()
+        rospy.sleep(2)
+        
+        left_group.go(wait = True)
+        rospy.sleep(2)
 
 if __name__ == '__main__':
     move_arm_node()
